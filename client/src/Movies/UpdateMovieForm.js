28968d2d2649aa59aa5c82ee0af55a72ede252ff
Neha Patel <neha.r.p@gmail.com>
2019-08-22 17:38:05 -0700

    added skeleton of functional component in UpdateMovieForm

---
+++ client/src/Movies/UpdateMovieForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import axios from 'axios';
+
+const UpdateMovieForm = () => {
+    return(
+        <div>
+            Movie Form Here
+        </div>
+    )
+}
+
+export default UpdateMovieForm;
