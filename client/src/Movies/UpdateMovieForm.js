b41c8d5348bf66697368071a2aa584a96428ef0e
Neha Patel <neha.r.p@gmail.com>
2019-08-22 18:00:07 -0700

    set up useState for UpdateMovieForm

--- client/src/Movies/UpdateMovieForm.js
+++ client/src/Movies/UpdateMovieForm.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+const initialMovie = {
+    title: '',
+    director: '',
+    metascore: '',
+    stars: [],
+}
+
 const UpdateMovieForm = () => {
+const [ movie, setMovie ] = useState(initialMovie)
+
     return(
         <div>
             Movie Form Here
